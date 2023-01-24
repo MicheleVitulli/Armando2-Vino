de8fffa5ca7e4203d2b01813f3434c2c42a08431
Giacomo Bacchetta <giacomo.bacchetta4@gmail.com>
2023-01-24 09:52:35 +0100

    Update 05_Gestione ordini 🥳.py

--- pages/05_Gestione ordini 🥳.py
+++ pages/05_Gestione ordini 🥳.py
@@ -178,7 +178,7 @@ with tab2:
   for doc in docs_resi:
     reso = doc.to_dict()['reso']
     st.write(reso)
-    for i in dict_resi:
+    for i in reso:
       resi.append({"Data Reso" : doc.to_dict()['data'],"Nome": doc.to_dict()['nome'],  "Vino": i , "Quantità": dict_resi[i]}) 
   st.write(resi)
 
