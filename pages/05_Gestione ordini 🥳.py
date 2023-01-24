7a29cc803a839a651f56d6a720f262762f77bcb3
Giacomo Bacchetta <giacomo.bacchetta4@gmail.com>
2023-01-24 10:26:00 +0100

    Update 05_Gestione ordini 🥳.py

--- pages/05_Gestione ordini 🥳.py
+++ pages/05_Gestione ordini 🥳.py
@@ -142,8 +142,10 @@ with tab2:
       for doc in docs_ordini:
         if reso_id == doc.id:
           ordine = doc.to_dict()['ordinato']
-          prodotti_resi = ordine.keys()
-      vini_resi = col2.multiselect('Scegli il prodotto da rendere', prodotti_resi)
+          prodotti = ordine.keys()
+          st.write(prodotti)
+
+      vini_resi = col2.multiselect('Scegli il prodotto da rendere', prodotti)
 
       dict_resi = {}
       for vino in vini_resi:
