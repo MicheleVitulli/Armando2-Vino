7596ac937e53379aee40d51643a40f71cf6cf460
Giacomo Bacchetta <giacomo.bacchetta4@gmail.com>
2023-01-25 09:43:11 +0100

    Update 05_Ricevimenti 🥳.py

--- pages/05_Ricevimenti 🥳.py
+++ pages/05_Ricevimenti 🥳.py
@@ -157,6 +157,7 @@ with tab2:
         dict_resi = {}
         for vino in vini_resi:
           q_reso = col2.number_input('Quantità di reso di {}'.format(' '.join(vino.split('-'))), key=str(vino), min_value=0, step=1)
+          dict_resi[vino] = q_reso
 
       aggiorna_reso = col2.button('Registra reso')
 
