f5240c9986782842b370e318c609d6805e1989af
Giacomo Bacchetta <giacomo.bacchetta4@gmail.com>
2023-01-25 09:28:39 +0100

    Update 05_Ricevimenti 🥳.py

--- pages/05_Ricevimenti 🥳.py
+++ pages/05_Ricevimenti 🥳.py
@@ -158,9 +158,9 @@ with tab2:
         for vino in vini_resi:
           q_ord = vini_ordinato_dic[vino]
           q_reso = col2.number_input('Quantità di reso di {}'.format(' '.join(vino.split('-'))), key=str(vino), min_value=0, step=1)
-          if q_reso > q_ord:
+          if q_reso and q_reso > q_ord:
             col2.warning('⚠️ Quantità da rendere non disponibile')
-          else:
+          elif q_reso and q_reso <= q_ord:
             dict_resi[vino] = q_reso
 
       aggiorna_reso = col2.button('Registra reso')
