85e77ab6eea54afa3324250f3c0c556711907ffb
Michele Vitulli <github@jesap.it>
2023-01-29 17:41:52 +0100

    Update 01_Inserimento 📝.py

--- pages/01_Inserimento 📝.py
+++ pages/01_Inserimento 📝.py
@@ -10,7 +10,7 @@ import time
 from functions import check_password
 
 # --- Layout comune a tutte le pagine
-#st.set_page_config(page_title='Armando 2.0', layout = 'wide', page_icon = '🍷', initial_sidebar_state = 'auto')
+st.set_page_config(page_title='Armando 2.0', layout = 'wide', page_icon = '🍶', initial_sidebar_state = 'auto')
 hide_streamlit_style = """
 	            <style>
 	            #MainMenu {visibility: hidden;}
