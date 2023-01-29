01d5633c953040f24776af45cf41044cb146f3b6
Michele Vitulli <github@jesap.it>
2023-01-29 17:48:16 +0100

    icon

--- pages/02_Vendita 💰.py
+++ pages/02_Vendita 💰.py
@@ -8,7 +8,7 @@ from zoneinfo import ZoneInfo
 from functions import check_password
 
 # --- Layout comune a tutte le pagine
-st.set_page_config(page_title='Armando 2.0', layout = 'wide', page_icon = '', initial_sidebar_state = 'auto')
+st.set_page_config(page_title='Le Bon Ton', layout = 'wide', page_icon = '', initial_sidebar_state = 'auto')
 hide_streamlit_style = """
 	            <style>
 	            #MainMenu {visibility: hidden;}
